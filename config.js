a554f2e14f44fca1e57fa99fe1473ecbb243da47
irsyadhanafi <syadhnf@gmail.com>
2026-02-24 18:51:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -79,8 +79,8 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://files.catbox.moe/69rw8l.mp3", // Music streaming URL
-        startText: "🎵 Play Music",        // Button text to start music
-        stopText: "🔇 Stop Music",         // Button text to stop music
+        startText: "🔊",        // Button text to start music
+        stopText: "🔇",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
